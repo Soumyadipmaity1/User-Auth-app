27d1681e2badbce179e45aab25d1677d577c0491
smilingsoumya1 <soumyadipmaityjee1@gmail.com>
2024-06-08 22:19:31 +0530

    chore: Add login functionality and improve user experience

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,42 @@
 "use client";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
+import toast from "react-hot-toast";
 function page() {
+  const router = useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: "",
     username: "",
   });
-  const onLogin = async () => {};
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+  const onLogin = async () => {
+    try{
+      setLoading(true);
+      const response = await axios.post("/api/users/login", user);
+      console.log("Login Success", response.data);
+      toast.success("Login successful");
+      router.push("/profile")
+    }  catch (error: any) {
+console.log("loin failed", error.message)
+toast.error(error.message)
+    }
+  }
+
+
+    useEffect(() => {
+      if (user.email.length > 0 && user.password.length > 0) {
+        setButtonDisabled(false);
+      } else {
+        setButtonDisabled(true);
+      }
+  }, [user]);
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-5 ">
-      <h1 className=" p-5 text-3xl pb-20">Login</h1>
+      <h1 className=" p-5 text-3xl pb-20">{loading ? "Processing" : "Login"}</h1>
       <div className="p-1 ">
         <label htmlFor="email">Email</label>
         <input
