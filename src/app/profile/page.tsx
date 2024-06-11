aa4023e01f67e4d4b2e3e04293ff1533eb072524
smilingsoumya1 <soumyadipmaityjee1@gmail.com>
2024-06-12 02:02:46 +0530

    chore: Add logout button to profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,37 @@
+"use client";
+import Link from "next/link";
+import axios from "axios";
+import toast from "react-hot-toast";
+import router, { useRouter } from "next/navigation";
 
 
-import React from 'react'
 
 function ProfilePage() {
+  const router = useRouter();
+const logout = async () => {
+
+  try{
+    await axios.get("/api/users/logout");
+    toast.success("Logout successful");
+    router.push("/login");
+  }
+  catch(error: any){
+    console.log("logout failed", error.message);
+    toast.error(error.message);
+  }
+}
+
   return (
     <div>
         <h1>Profile Page</h1>
+        <hr/>
+        
+        <button
+          className="p-2 border px-4 hover:bg-gray-300 hover:text-black font-semibold mt-10 border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+          onClick={logout}
+        >
+          Log Out
+        </button>
     </div>
   )
 }
