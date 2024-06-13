43d00e6a89c5826acf16a3cc92e7c6b8130c2784
Soumyadip Maity <22053029@kiit.ac.in>
2024-06-13 13:22:09 +0530

    feat: Update user route to retrieve user data from token

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -1,21 +1,22 @@
 import { getTokenFromData } from "@/helpers/getDataFromToken";
-
 import { NextRequest, NextResponse } from "next/server";
 import User from "@/models/userModel"
+import Connect from "@/dbconfig/dbconfig";
+ Connect();
 
-import {Connect} from "@/dbconfig/dbconfig";
-import { connect } from "tls";
-Connect();
-
-export aysnc function GET(request: NextRequest) {
+export async function GET(request: NextRequest) {
     try{
-        const userId = await getDataFromToken(request);
-         const user = await User.findOne({_id: userId});
+        const userId = await getTokenFromData(request);
+         const user = await User.findOne({_id: userId}).
+         select("-password ");
+         return NextResponse.json({
+            message: "User found",
+            data: user
+         });
          
 
-select("-password") ;        })
     }
     catch(error: any){
-        return NextResponse.json({ error: error.message }, {status: 500});
+        return NextResponse.json({ error: error.message }, {status: 400});
     }
 }
