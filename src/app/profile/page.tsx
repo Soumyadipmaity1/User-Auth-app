7a3ac7f34ce7726eb80c10897e13045af4b594a1
Soumyadip Maity <22053029@kiit.ac.in>
2024-06-13 13:22:25 +0530

    chore: Update profile page with logout button and user details retrieval

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,11 +3,13 @@ import Link from "next/link";
 import axios from "axios";
 import toast from "react-hot-toast";
 import router, { useRouter } from "next/navigation";
+import { useState } from "react";
 
 
 
 function ProfilePage() {
   const router = useRouter();
+  const [data, setData] = useState("nothing");
 const logout = async () => {
 
   try{
@@ -20,11 +22,20 @@ const logout = async () => {
     toast.error(error.message);
   }
 }
-
+const getUserDetails = async () => {
+  const res = await axios.get("/api/users/me");
+  console.log(res.data);
+setData(res.data.data._id);
+} 
   return (
     <div>
-        <h1>Profile Page</h1>
+        <h1>Profile </h1>
         <hr/>
+        <p>Profile page</p>
+        <h2 className="rounded p-3 bg-orange-500">{data=="nothing" ? "Nothing" : <Link href={`/profile/${data}`}>
+        {data}
+        </Link>}</h2>
+
         
         <button
           className="p-2 border px-4 hover:bg-gray-300 hover:text-black font-semibold mt-10 border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
@@ -32,6 +43,11 @@ const logout = async () => {
         >
           Log Out
         </button>
+        <button
+          className="p-2 border px-4 hover:bg-gray-300 hover:text-black font-semibold mt-10 border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+          onClick={getUserDetails}
+        >
+Get user details        </button>
     </div>
   )
 }
