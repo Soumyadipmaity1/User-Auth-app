b38d20fd6e98c6bde541c80d5e7144b45d6f5830
Soumyadip Maity <22053029@kiit.ac.in>
2024-06-13 13:22:17 +0530

    feat: Refactor getTokenFromData to handle empty token value

--- src/helpers/getDataFromToken.ts
+++ src/helpers/getDataFromToken.ts
@@ -4,8 +4,8 @@ import { NextRequest } from "next/server";
 
 export const getTokenFromData = (request: NextRequest) => {
     try{
-        const token = request.cookies.get("token")?.value || "";
-      jwt.verify(token, process.env.TOKEN_SECRET!);
+        const token = request.cookies.get("token")?.value || ' ';
+      const decodedToken:any = jwt.verify(token, process.env.TOKEN_SECRET!);
         return decodedToken.id;
     }
     catch(error: any){
