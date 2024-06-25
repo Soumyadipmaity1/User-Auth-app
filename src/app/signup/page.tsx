8fe41f47e422a092a3f47c495bdb21fb5b68f757
Soumyadip Maity <22053029@kiit.ac.in>
2024-06-25 17:01:58 +0530

    chore: Update signup page with username, email, and password input styling

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -4,6 +4,8 @@ import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
+import { FaUser, FaEnvelope, FaLock } from "react-icons/fa";
+
 // import { Response } from 'next/server';
 function page() {
   const router = useRouter();
@@ -14,78 +16,78 @@ function page() {
   });
   const [buttonDisabled, setButtonDisabled] = React.useState(false);
   useEffect(() => {
-    if (user.email.length>0 && user.password.length>0 && user.username.length>0) {
+    if (user.email.length > 0 && user.password.length > 0 && user.username.length > 0) {
       setButtonDisabled(false);
     } else {
       setButtonDisabled(true);
     }
-  },[user]);
+  }, [user]);
   const [loading, setLoading] = React.useState(false);
   const onSignUp = async () => {
-    try{
-setLoading(true);
-const response =await axios.post("/api/users/signup", user)
-console.log("sign up success",response.data);
-router.push("/login");
-}
-    catch(error: any){
-console.log(error)
-console.log("signup failed",error.message);
-toast.error(error.message);
-}
-    finally
- {
-  setLoading(false);
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/signup", user)
+      console.log("sign up success", response.data);
+      router.push("/login");
+    }
+    catch (error: any) {
+      console.log(error)
+      console.log("signup failed", error.message);
+      toast.error(error.message);
+    }
+    finally {
+      setLoading(false);
 
- }
+    }
   };
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-5 ">
-      <h1 className=" p-5 text-3xl pb-20">{loading ? "Processing" : "SignUp"}</h1>
-      <div className="p-1 ">
-        <label htmlFor="username">username</label>
-        <input
-          type="username"
-          id="username"
-          value={user.username}
-          onChange={(e) => setUser({ ...user, username: e.target.value })}
-          placeholder=" Enter your username"
-          className="p-2 mx-4 border  rounded-lg text-black mb-4 focus:outline-none focus:border-gray-600"
-        />
+      <section className="flex flex-col items-center justify-center min-h-screen ">
+      <div className="flex flex-col items-center justify-center bg-[#181818] px-12 py-5 pb-10 form-shadow rounded-xl">
+        <h1 className="p-5 font-bold text-orange-500 text-3xl pb-20">{loading ? "Processing" : "Sign Up"}</h1>
+        <div className="p-1 flex items-center justify-center">
+          <FaUser className="text-orange-500 mb-4 text-2xl" />
+          <input
+            type="text"
+            value={user.username}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUser({ ...user, username: e.target.value })}
+            placeholder="Enter your username"
+            className="p-2 mx-4 border rounded-lg text-black mb-4 focus:outline-none focus:border-gray-600"
+          />
+        </div>
+        <div className="p-1 flex items-center justify-center">
+          <FaEnvelope className="text-orange-500 mb-4 text-2xl" />
+          <input
+            type="email"
+            value={user.email}
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+            placeholder="Enter your email"
+            className="p-2 mx-4 border rounded-lg text-black mb-4 focus:outline-none focus:border-gray-600"
+          />
+        </div>
+        <div className="p-1 flex items-center justify-center">
+          <FaLock className="text-orange-500 mb-4 text-2xl" />
+          <input
+            type="password"
+            value={user.password}
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+            placeholder="Enter your password"
+            className="p-2 mx-4 border rounded-lg text-black mb-4 focus:outline-none focus:border-gray-600"
+          />
+        </div>
+        <div className="flex flex-col justify-center">
+          <button
+            className={`p-2 border px-4 hover:bg-white hover:text-black font-semibold mt-10 border-white rounded-lg mb-4 focus:outline-none focus:border-gray-600 ${buttonDisabled ? 'opacity-50 cursor-not-allowed' : ''}`}
+            onClick={onSignUp}
+            disabled={buttonDisabled}
+          >
+            {buttonDisabled ? "No signup" : "Sign up"}
+          </button>
+          <br />
+          <Link className="hover:underline p-1 rounded-md px-3" href="/login">Go to Login Page</Link>
+        </div>
       </div>
-      <div className="p-1 ">
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          value={user.email}
-          onChange={(e) => setUser({ ...user, email: e.target.value })}
-          placeholder=" Enter your email"
-          className="p-2 mx-4 border  rounded-lg text-black mb-4 focus:outline-none focus:border-gray-600"
-        />
-      </div>
-      <div className="p-1 ">
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          value={user.password}
-          onChange={(e) => setUser({ ...user, password: e.target.value })}
-          placeholder=" Enter your password"
-          className="p-2 mx-4 border  rounded-lg text-black mb-4 focus:outline-none focus:border-gray-600"
-        />
-      </div>
-      <div className="flex flex-col justify-center">
-        <button
-          className="p-2 border px-4 hover:bg-gray-300 hover:text-black font-semibold mt-10 border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
-          onClick={onSignUp}
-        >
-{buttonDisabled ? "No signup" : "signup"}        </button> <br />
-        <Link href="/login">Go to Login Page</Link>
-
-      </div>
-    </div>
-  );
+      </section>
+    );
 }
 
 export default page;
