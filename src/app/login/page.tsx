397cebe688d4063455b1732f99cc36f5eed24e94
Soumyadip Maity <22053029@kiit.ac.in>
2024-06-25 17:01:45 +0530

    chore: Update login page with email and password input styling

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,6 +4,8 @@ import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
+import { FaEnvelope, FaLock } from "react-icons/fa";
+
 function page() {
   const router = useRouter();
   const [user, setUser] = React.useState({
@@ -35,40 +37,41 @@ toast.error(error.message)
       }
   }, [user]);
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-5 ">
-      <h1 className=" p-5 text-3xl pb-20">{loading ? "Processing" : "Login"}</h1>
-      <div className="p-1 ">
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          value={user.email}
-          onChange={(e) => setUser({ ...user, email: e.target.value })}
-          placeholder=" Enter your email"
-          className="p-2 mx-4 border  rounded-lg text-black mb-4 focus:outline-none focus:border-gray-600"
-        />
-      </div>
-      <div className="p-1 ">
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          value={user.password}
-          onChange={(e) => setUser({ ...user, password: e.target.value })}
-          placeholder=" Enter your password"
-          className="p-2 mx-4 border  rounded-lg text-black mb-4 focus:outline-none focus:border-gray-600"
-        />
-      </div>
-      <div className="flex flex-col justify-center">
-        <button
-          className="p-2 border px-4 hover:bg-gray-300 hover:text-black font-semibold mt-10 border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
-          onClick={onLogin}
-        >
-          Login
-        </button><br />
-        <Link href="/signup">Go to SignUp Page</Link>
-      </div>
+    <div className="flex flex-col items-center justify-center min-h-screen py-5">
+    <h1 className="p-5 text-3xl pb-20">{loading ? "Processing" : "Login"}</h1>
+    <div className="p-1 flex items-center w-72">
+      <FaEnvelope className="text-orange-500 mr-2 text-2xl" />
+      <input
+        type="email"
+        id="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="Enter your email"
+        className="p-2 border rounded-lg text-black mb-4 focus:outline-none focus:border-gray-600 w-full"
+      />
     </div>
+    <div className="p-1 flex items-center w-72">
+      <FaLock className="text-orange-500 mr-2 text-2xl" />
+      <input
+        type="password"
+        id="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="Enter your password"
+        className="p-2 border rounded-lg text-black mb-4 focus:outline-none focus:border-gray-600 w-full"
+      />
+    </div>
+    <div className="flex flex-col justify-center">
+      <button
+        className="p-2 border px-4 hover:bg-gray-300 hover:text-black font-semibold mt-10 border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        onClick={onLogin}
+      >
+        Login
+      </button>
+      <br />
+      <Link href="/signup">Go to Sign Up Page</Link>
+    </div>
+  </div>
   );
 }
 
